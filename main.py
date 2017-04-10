18483707e0826a0cf6852d9753635f8960dd5f15
Alex Pereverzyev <pereverzev.alex@gmail.com>
2017-04-10 21:20:22 +0300

    add tests with scikit-learn

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+from sklearn import linear_model
 from predictor import LinearRegression
 from classifier import LogisticRegression
 from newtons_classifier import NewtonsClassifier
 from training_sets import *
 
-
 if __name__ == "__main__":
     test_regression('Linear Regression', 1, 100, 0.00001, 1.5,
                     TrainingSet1, ValidationSet1, 0, LinearRegression)
 
+    test_regression_skl('Linear Regression (SKL)', 1.5, TrainingSet1,
+                        ValidationSet1, 0, linear_model.LinearRegression)
+
     test_regression('Logistic Regression 1', 2, 100, 0.0005, .1,
                     TrainingSet2, ValidationSet2, 0, LogisticRegression)
+
+    test_regression_skl('Logistic Regression 1 (SKL)', .1, TrainingSet2,
+                        ValidationSet2, 0, linear_model.LogisticRegression)
 
     test_regression('Logistic Regression 2', 2, 100, 0.5, .1,
                     TrainingSet4, ValidationSet4, 0, LogisticRegression)
 
+    test_regression_skl('Logistic Regression 2 (SKL)', .1, TrainingSet4,
+                        ValidationSet4, 0, linear_model.LogisticRegression)
+
     test_regression('Logistic Regression 2 (N-R)', 2, 5, 1, .1,
                     TrainingSet4, ValidationSet4, 0, NewtonsClassifier)
 
-    # test_regression('Failure Probability', 11, 1000, 0.0000001, .1,
-    #                 TrainingSet3, ValidationSet3, 0, LogisticRegression)
+    # test_regression('Slowness Probability', 11, 1000, 0.0000001, .1,
+    #                 TrainingSet3, ValidationSet3, 100, LogisticRegression)
+
+    # test_regression_sk('Slowness Probability (SK)', .1, TrainingSet3,
+    #                    ValidationSet3, 100, linear_model.LogisticRegression)
--- training_sets.py
+++ training_sets.py
@@ -1,12 +1,13 @@
 
 from data_tools import DataIterator
-
+from sklearn import linear_model
+from functools import reduce
 
 TrainingSet1 = """
         test_data/linear.csv|@N,one,X,Y|:6"""
 
 ValidationSet1 = """
-        test_data/linear.csv|@N,one,X,Y|:7"""
+        test_data/linear.csv|@N,one,X,Y|7:"""
 
 TrainingSet2 = """
         test_data/logistic.csv|@N,one,X1,X2,Y|:9"""
@@ -30,7 +31,7 @@ TrainingSet3 = """
     """
 
 ValidationSet3 = """
-        data/CLR-Garbage Collection-GC Time Spent.csv|@date,one,value|500:600;
+        data/CLR-Garbage Collection-GC Time Spent.csv|@date,one,value|500:1000;
         data/CLR-Locks and Threads-Current Physical Threads.csv|@date,value;
         data/Hardware Resources-CPU-Busy.csv|@date,value;
         data/Hardware Resources-Memory-Used.csv|@date,value;
@@ -54,9 +55,9 @@ ValidationSet4 = """
 def test_regression(name, features, iterations, learning, precision,
                     training_set, validation_set, details_count,
                     algorithm):
+    print('-' * 20, name, '-' * 20)
     with DataIterator(training_set) as data_generator:
         feed = list(data_generator)
-    print('-' * 20, name, '-' * 20)
     regression = algorithm(features, iterations, learning, precision)
     regression.batch_train(feed)
     print(regression)
@@ -66,3 +67,30 @@ def test_regression(name, features, iterations, learning, precision,
     print('E  = {0} %'.format(regression.calc_training_error(feed)))
     for d in (d for i, d in enumerate(feed) if i < details_count):
         print('Ye = {0}, Yr = {1}'.format(regression.predict(d), d[-1]))
+
+
+def test_regression_skl(name, precision, training_set, validation_set, details_count,
+                       algorithm):
+    print('-' * 20, name, '-' * 20)
+    with DataIterator(training_set) as data_generator:
+        feed = list(data_generator)
+    X = [f[1:-1] for f in feed]
+    Y = [f[-1] for f in feed]
+    regression = algorithm()
+    regression.fit(X, Y)
+    print('Y = ', regression.coef_)
+    print('Et = ?')
+    # print('Et = {0} %'.format(regression.calc_training_error(feed)))
+    with DataIterator(validation_set) as data_generator:
+        feed = list(data_generator)
+    # todo: refactor
+    missed_count = list(map(lambda d:
+                        int(abs(regression.predict(d[1:-1])[0] - d[-1]) >  precision),
+                        feed))
+    error = reduce(lambda x, y: x + y, missed_count) / len(missed_count)
+    error *= 100
+    print('E  = {0} %'.format(error))
+
+    for d in (d for i, d in enumerate(feed) if i < details_count):
+        x = d[1:-1]
+        print('Ye = {0}, Yr = {1}'.format(regression.predict(x)[0], d[-1]))
